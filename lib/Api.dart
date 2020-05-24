66d9b007f56f0b32f9d6db442942f40437e8feb0
Fernando Sinigaglia <sinigagliafernando@gmail.com>
2020-05-24 19:48:59 -0300

    commit

--- lib/Api.dart
+++ lib/Api.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 
 import 'package:youtube/model/Video.dart';
 
-const CHAVE_YOUTUBE_API = "";
-const ID_CANAL = "";
+const CHAVE_YOUTUBE_API = "AIzaSyBco4lD-dyANAnW4_NgveSg0pZC3VsSqkY";
+const ID_CANAL = "UC7k2B_XMew_FmxDV4LXiI6Q";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 class Api {
